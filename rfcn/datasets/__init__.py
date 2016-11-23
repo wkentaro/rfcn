4ea3f24d14dbbbcd215b8f9ff252973330362538
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-11-24 04:53:30 +0900

    Cleanup datasets module

--- rfcn/datasets/__init__.py
+++ rfcn/datasets/__init__.py
@@ -1,3 +1,5 @@
 from rfcn.datasets.instance_segmentation import InstanceSegmentationDatasetBase  # NOQA
 
-from rfcn.datasets.coco_segmentation import CocoSegmentaionDataset  # NOQA
+from rfcn.datasets.coco import CocoSegmentaionDataset  # NOQA
+
+from rfcn.datasets.pascal import PascalInstanceSegmentationDataset  # NOQA
